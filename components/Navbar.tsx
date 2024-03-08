e9c206b52f1272ed41333af811140113120d33d5
emmspy <emilysky48@gmail.com>
2024-03-07 22:24:34 -0500

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <nav className='flexBetween navbar'>
+        <div className='flex-1 flexStart gap-10'>
+            <ul>
+                
+            </ul>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
